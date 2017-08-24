79e653c8ac9bfcd9d12e29feb8a63b846b022a79
Dan Mercer <danielmercer1@gmail.com>
2017-08-23 21:02:01 -0600

    Added generate() and tests.

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log("Hello world");
+var Immutable = require("immutable");
+var Map = Immutable.Map;
+var OrderedMap = Immutable.OrderedMap;
+var is = Immutable.is;
+
+// TODO: eventually replace these with Immutable.isImmutable and .isOrdered -
+// see https://github.com/facebook/immutable-js/issues/1165#issuecomment-292650855
+var isImmutable = maybeImmutable => Immutable.Iterable.isIterable(maybeImmutable);
+var isOrdered = maybeOrdered => maybeOrdered instanceof OrderedMap;
+
+var deepEqual = require("deep-equal");
+
+function generate(before, after) {
+	var ordered = before instanceof OrderedMap;
+	var diff = ordered ? new OrderedMap() : new Map();
+
+	return diff.withMutations(function (mutableDiff) {
+		var remainingAfter = after.withMutations(function (mutableAfter) {
+
+			before.forEach(function(bVal, key) {
+				// Ignore non-string keys
+				if (typeof key !== "string") return;
+
+				// Detect deletion
+				if (!after.has(key)) {
+					mutableDiff.set(key, null);
+					return;
+				}
+				var aVal = after.get(key);
+
+				// Recursively compare maps:
+				if (Map.isMap(bVal) && Map.isMap(aVal)) {
+					var subdiff = generate(bVal, aVal);
+					if (!subdiff.isEmpty()) {
+						mutableDiff.set(key, subdiff);
+					}
+
+					// Not both maps, so just compare values deeply
+				} else if (!deepEquals(bVal, aVal)) {
+					mutableDiff.set(key, aVal);
+				}
+
+				// Track which keys in `after` we've already processed
+				mutableAfter.delete(key);
+			});
+		});
+
+		// Record any additions (the remaining entries in remainingAfter)
+		// (we call toObject to make sure all the keys are strings)
+		mutableDiff.merge(new Map(remainingAfter.toObject()));
+	});
+}
+
+module.exports = {
+	generate: generate,
+}
+
+function deepEquals(a, b) {
+	if (isImmutable(a)) {
+		return !isImmutable(b) || is(a, b);
+	} else {
+		return isImmutable(b) || deepEqual(a, b, {strict: true});
+	}
+}
---
+++ test/index.test.js
@@ -0,0 +1,174 @@
+var assert = require("assert");
+var Immutable = require("immutable");
+var Map = Immutable.Map;
+var List = Immutable.List;
+
+// immutable-merge-patch
+var imp = require("../index");
+
+describe("generate", () => {
+
+	it("should properly detect a value change", () => {
+		var a = Map({
+			a: 1,
+			b: 2,
+			c: 3
+		});
+		var b = Map({
+			a: 1,
+			b: 5,
+			c: 3
+		});
+
+		var diff = imp.generate(a, b);
+		assert.deepEqual(
+			diff.toObject(),
+			{
+				b: 5,
+			}
+		);
+		var otherWay = imp.generate(b, a);
+		assert.deepEqual(
+			otherWay.toObject(),
+			{
+				b: 2,
+			}
+		);
+	});
+
+	it("should properly detect a value insertion", () => {
+		var a = Map({
+			p: 1,
+		});
+		var b = Map({
+			p: 1,
+			q: 3
+		});
+
+		var diff = imp.generate(a, b);
+		assert.deepEqual(
+			diff.toObject(),
+			{
+				q: 3,
+			}
+		);
+	});
+
+	it("should properly detect a value removal", () => {
+		var a = Map({
+			p: 1,
+			q: 3
+		});
+		var b = Map({
+			p: 1
+		});
+
+		var diff = imp.generate(a, b);
+		assert.deepEqual(
+			diff.toObject(),
+			{
+				q: null,
+			}
+		);
+	});
+
+	it("should properly recurse", () => {
+		var a = Map({
+			a: 1,
+			b: 2,
+			c: 3
+		});
+		var b = Map({
+			a: 1,
+			b: 5,
+			c: 3
+		});
+		var aP = Map({
+			child: a
+		});
+		var bP = Map({
+			child: b
+		});
+
+		var childDiff = imp.generate(a, b);
+		var parentDiff = imp.generate(aP, bP);
+
+		assert.deepEqual(parentDiff.toJS(), {
+			child: childDiff.toJS()
+		});
+	});
+
+	describe("non-Map object values", () => {
+
+		it("should properly detect equivalent structures", () => {
+			// Non-Immutable structures
+			var a = Map({
+				p: 1,
+				q: [1, 2, 4]
+			});
+			var b = Map({
+				p: 1,
+				q: [1, 2, 4]
+			});
+
+			var diff = imp.generate(a, b);
+			assert.deepEqual(
+				diff.toObject(), {}
+			);
+
+			// Immutable structures
+			var c = Map({
+				p: 1,
+				q: List([1, 2, 4])
+			});
+			var d = Map({
+				p: 1,
+				q: List([1, 2, 4])
+			});
+
+			var diff2 = imp.generate(c, d);
+			assert.deepEqual(
+				diff2.toObject(), {}
+			);
+		});
+
+		it("should properly detect changes", () => {
+			// Non-Immutable structures
+			var a = Map({
+				p: 1,
+				q: [1, 2, 4]
+			});
+			var b = Map({
+				p: 1,
+				q: [1, 3, 4]
+			});
+
+			var diff = imp.generate(a, b);
+			assert.deepEqual(
+				diff.toObject(),
+				{
+					q: [1, 3, 4]
+				}
+			);
+
+			// Immutable structures
+			var c = Map({
+				p: 1,
+				q: List([1, 2, 4])
+			});
+			var d = Map({
+				p: 1,
+				q: List([1, 3, 4])
+			});
+
+			var diff2 = imp.generate(c, d);
+			assert.deepEqual(
+				diff2.toObject(),
+				{
+					q: List([1, 3, 4])
+				}
+			);
+		});
+	});
+
+});
